196e4118f2f92129a61dee82b77dda387ace2c83
CustCoding <custodiotitossetitosse@gmail.com>
2026-07-22 19:14:08 +0200

    feat(frontend): implement public home page route

---
+++ apps/frontend/app/(public)/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import MainEditorialContent from "@/components/layout/home/Home";
+
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <MainEditorialContent/>
+      <Footer/>
+    </>
+
+
+    
+  );
+}
